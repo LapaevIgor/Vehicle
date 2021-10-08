52e5f0ab27a5b24b3e67ca0ee95668fe92738de6
Igor Lapaev <igor.lapaev@hiqo-solutions.com>
2021-10-08 18:22:48 +0300

    Add notifications service to docker-compose

--- update.bat
+++ update.bat
@@ -1,6 +1,7 @@
 docker-compose down
 docker image rm vehicle/frontend
 docker image rm vehicle/backend
+docker image rm vehicle/notifications_sevice
 
 git pull
 
@@ -9,5 +10,9 @@ docker build -t vehicle/frontend .
 cd ..
 
 docker build -t vehicle/backend .
+
+cd Vehicle.NotificationsService
+docker build -t vehicle/notifications_service .
+cd ..
 
 docker-compose up -d
